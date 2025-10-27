513ffa7014c73cef804c127bcec65bebe2ee808a
Brendon <bluicien@yahoo.com>
2025-10-27 23:25:44 +1300

    Updated connection string

---
+++ deploy.sql
@@ -0,0 +1,69 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251025045048_InitialCreate'
+)
+BEGIN
+    CREATE TABLE [Feedback] (
+        [UserId] int NOT NULL IDENTITY,
+        [Username] nvarchar(max) NOT NULL,
+        [Company] nvarchar(max) NULL,
+        [Position] nvarchar(max) NULL,
+        [Rating] int NOT NULL,
+        [Message] nvarchar(max) NULL,
+        [UserIpAddress] int NULL,
+        [IsApproved] bit NOT NULL,
+        [CreatedAt] datetime2 NOT NULL,
+        [UpdatedAt] datetime2 NOT NULL,
+        CONSTRAINT [PK_Feedback] PRIMARY KEY ([UserId])
+    );
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251025045048_InitialCreate'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251025045048_InitialCreate', N'9.0.10');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251027063512_UpdateFeedbackModel'
+)
+BEGIN
+    EXEC sp_rename N'[Feedback].[UserId]', N'FeedbackId', 'COLUMN';
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251027063512_UpdateFeedbackModel'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251027063512_UpdateFeedbackModel', N'9.0.10');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251027071855_InitializeAzureDb'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251027071855_InitializeAzureDb', N'9.0.10');
+END;
+
+COMMIT;
+GO
+
