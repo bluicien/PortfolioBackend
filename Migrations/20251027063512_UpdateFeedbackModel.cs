286c3c250665967822ad2e510a6d92e8355e838f
Brendon <bluicien@yahoo.com>
2025-10-27 19:39:59 +1300

    Updated database migration

---
+++ Migrations/20251027063512_UpdateFeedbackModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFeedbackModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Feedback",
+                newName: "FeedbackId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeedbackId",
+                table: "Feedback",
+                newName: "UserId");
+        }
+    }
+}
